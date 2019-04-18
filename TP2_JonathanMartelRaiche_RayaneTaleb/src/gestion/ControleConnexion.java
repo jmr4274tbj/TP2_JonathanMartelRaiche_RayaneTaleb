4accf1edd7c8666954566f4b6ce840e49b821042
Rayane Taleb <rayane.taleb@hotmail.com>
2019-04-18 09:34:20 -0400

    Ajout code classe ControleConnexion

--- TP2_JonathanMartelRaiche_RayaneTaleb/src/gestion/ControleConnexion.java
+++ TP2_JonathanMartelRaiche_RayaneTaleb/src/gestion/ControleConnexion.java
@@ -1,10 +1,59 @@
 package gestion;
 
+import java.sql.*;
+
+import javax.swing.JOptionPane;
+
 public class ControleConnexion {
+	/**  
+	 * L'intérêt d'une connexion statique est de disposer d'une connexion unique durant toute une session.  
+	 */
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static Connection laConnexion; 
+	//private static String  url = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=c:/bd/employes.accdb"; 
+	private static String  url = "jdbc:sqlite:sqlite/db/biblioteque_musicale.db";
+	
+	/** 
+	 * Établit la connexion à la BDD si elle n'existe pas. Attention, la connexion ne doit pas être fermée 
+	 */	
+	public static void connecter() {  
+		try{   
+			if(laConnexion == null || laConnexion.isClosed()) {      
+			//Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");  
+			Class.forName("org.sqlite.JDBC");
+			laConnexion= DriverManager.getConnection(url);    
+			JOptionPane.showMessageDialog(null, "Connect\u00E9 à la BD", "ALERTE",                      
+					JOptionPane.INFORMATION_MESSAGE);    
+			}    
+		
+		} catch(ClassNotFoundException e) { 
+			System.out.println("ERREUR : " + e);
+			
+		} catch(SQLException sqle) {        
+			System.out.println("ERREUR : " + sqle);
+		}   
+	} 
+	  
+	public static void fermerSession(){  
+		try{   
+			if (laConnexion!=null  && !laConnexion.isClosed()) {
+				laConnexion.close();
+			} 
+		}  
+		catch(SQLException sqle){ 
+			System.out.println("ERREUR SQL : " + sqle);
+		} 
+	} 
+	
+	public static Connection getLaConnexion() {
+		return laConnexion; 
+		
+	} 
+	
+	//POUR TESTER
+	public static void main(String args[]) {
+		connecter();
 	}
+	 
 
 }
